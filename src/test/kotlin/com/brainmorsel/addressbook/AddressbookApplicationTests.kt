1f41636c8f403fef032079d3c219bfb32a2b6907
Ilya Makarov <brainmorsel@gmail.com>
2020-04-26 17:50:08 +0300

    init project

---
+++ src/test/kotlin/com/brainmorsel/addressbook/AddressbookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.brainmorsel.addressbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AddressbookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
